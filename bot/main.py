67343017e526ad42634e82ce0bd1f592f716db5e
Murayama2 <79716232+Murayama2@users.noreply.github.com>
2025-06-26 04:17:17 +0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,8 @@ import os
 import asyncio
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
+from handlers.user_handler import user_router
+from handlers.user_handler import gift_router
 from handlers.user_input_handler import register_user_handlers
 from utils.gift_utils import gift_monitor
 
@@ -12,6 +14,10 @@ CHAT_ID = os.getenv("CHAT_ID")  # channel ID or user ID
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+async def main():
+    dp.include_router(gift_router)
+    dp.include_router(user_router)
 
 # Register user input handler
 register_user_handlers(dp)
